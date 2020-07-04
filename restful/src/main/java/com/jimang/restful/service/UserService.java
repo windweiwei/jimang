5b9fa75b1a176f0296b56a7ce30f41f650f0c008
applicatin.yml <1013398588@qq.com>
2020-07-04 15:51:53 +0800

    第一次提交

---
+++ restful/src/main/java/com/jimang/restful/service/UserService.java
@@ -0,0 +1,16 @@
+package com.jimang.restful.service;
+
+import com.jimang.restful.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author jw
+ * @since 2020-07-02
+ */
+public interface UserService extends IService<User> {
+
+}
